9752538822e6ecf289c393e55936266da77075dd
anisimani <90652708+anisimani@users.noreply.github.com>
2023-01-30 10:14:07 +0100

    Ajout d'une métrique de durée

--- src/modele/iris_api.py
+++ src/modele/iris_api.py
@@ -2,10 +2,10 @@ from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 import json
-from prometheus_client import Counter
+from prometheus_client import Counter, Histogram
 
-predict_counter = Counter(
-    'predict_requests', 'Number of requests to the predict endpoint')
+predict_counter = Counter('predict_requests', 'Number of requests to the predict endpoint')
+predict_duration = Histogram('predict_duration_seconds', 'Time taken to handle a predict request', ['method'])
 
 with open('src/modele/iris_knn.pkl', 'rb') as file:
     knn = joblib.load(file)
@@ -17,6 +17,9 @@ def predict():
 
     # incrémente le compteur
     predict_counter.inc()
+
+    # obtenir la durée de la requête
+    predict_duration.labels('post').observe(time.time() - request_start)
 
     # obtenir les données de la requête
     data = request.get_json(force=True)
@@ -32,9 +35,14 @@ def predict():
     return jsonify(response)
 
 
-@app.route('/metrics', methods=['GET'])
+@app.route('/metrics/counter', methods=['GET'])
 def get_counter():
-    response = {'counter': predict_counter._value}
+    response = {'counter': int(predict_counter._value)}
+    return jsonify(response)
+
+@app.route('/metrics/duration', methods=['GET'])
+def get_duration():
+    response = {'duration': predict_duration._value}
     return jsonify(response)
 
 
