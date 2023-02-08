aed3009da059f7bff1b2bb3bf0b81471d166b04c
NidhalWers <nteyeb75@gmail.com>
2023-02-08 09:23:06 +0100

    metrics

--- src/modele/iris_api.py
+++ src/modele/iris_api.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 import json
+import prometheus_client
 from prometheus_client import Counter, Histogram
 import time
+import re
 
 predict_counter = Counter(
     'predict_requests', 'Number of requests to the predict endpoint')
@@ -27,9 +29,12 @@ def predict():
     # effectuer la prédiction
     prediction = knn.predict(data)
 
+    # labels
+    iris_labels = ['iris setosa', 'iris versicolor', 'iris virginica']
+
     # préparer la réponse
     prediction_list = json.dumps(prediction.tolist())
-    response = {'prediction': prediction_list}
+    response = {'prediction': iris_labels[prediction[0]]}
 
     # obtenir la durée de la requête
     predict_duration.labels('post').observe(time.time() - request_start)
@@ -40,14 +45,24 @@ def predict():
 
 @app.route('/metrics/counter', methods=['GET'])
 def get_counter():
-    response = {'counter': int(predict_counter._value)}
+    #response = {'counter': prometheus_client.generate_latest(predict_counter)}
+    counter_string = prometheus_client.generate_latest(
+        predict_counter).decode("utf-8")
+    match = re.search(r'predict_requests_total (\d+\.\d+)', counter_string)
+    counter_value = float(match.group(1))
+    response = {'counter': counter_value}
     return jsonify(response)
 
 
 @app.route('/metrics/duration', methods=['GET'])
 def get_duration():
-    response = {'duration': {
-        'sum': predict_duration._sum, 'count': predict_duration._count}}
+    response = {'duration': prometheus_client.generate_latest(
+        predict_duration).decode("utf-8")}
+    # return jsonify(response)
+    #counter_string = prometheus_client.generate_latest(predict_counter).decode("utf-8")
+    #match = re.search(r'predict_requests_total (\d+\.\d+)', counter_string)
+    #counter_value = float(match.group(1))
+    #response = {'duration': counter_value}
     return jsonify(response)
 
 
